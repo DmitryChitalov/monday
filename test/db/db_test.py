5048938e107b92e0c1d319c8984f75a251df31ad
admin <cdi9@yandex.ru>
2026-02-23 17:55:35 +0500

    87

--- test/db/db_test.py
+++ test/db/db_test.py
@@ -19,14 +19,14 @@ def test_create_row():
     assert row
 
 #
-# def test_accomodation_delete():
-#     preparing()
-#     result_before = select_all()
-#     delete_by_id(id=11)
-#     result_after = select_all()
-#     delete_by_id(id=11)
-#     assert len(result_before) != len(result_after)
-# #
+def test_accomodation_delete():
+    preparing()
+    result_before = select_all()
+    delete_by_id(id=11)
+    result_after = select_all()
+    delete_by_id(id=11)
+    assert len(result_before) != len(result_after)
+#
 # #
 # def test_accomodation_update():
 #     preparing()
